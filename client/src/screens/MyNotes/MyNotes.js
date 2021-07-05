a445c1ac5b242b440f19de162c572d8a8847ac0e
ginni-nand <ginni1671.cse18@chitkara.edu.in>
2021-07-05 15:35:19 +0530

    Created my screen Page

--- client/src/screens/MyNotes/MyNotes.js
+++ client/src/screens/MyNotes/MyNotes.js
@@ -1,7 +1,8 @@
 import React from "react";
+import MainScreen from "../../components/MainScreen";
 
 const MyNotes = () => {
-  return <div>my notes</div>;
+  return <MainScreen title="Welcome back Ginni Nand">My Notes</MainScreen>;
 };
 
 export default MyNotes;
