465003d30750e6f8ed01ab8d5cbe22cfe43a6762
ginni-nand <ginni1671.cse18@chitkara.edu.in>
2021-07-12 12:53:04 +0530

    Login Frontend part

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,15 @@ import "./bootstrap.min.css";
 import LandingPage from "./screens/LandingPage/LandingPage";
 import { BrowserRouter, Route } from "react-router-dom";
 import MyNotes from "./screens/MyNotes/MyNotes";
+import LoginScreen from "./screens/LoginScreen/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen/RegisterScreen";
 const App = () => (
   <BrowserRouter>
     <Header />
     <main>
       <Route path="/" component={LandingPage} exact />
+      <Route path="/login" component={LoginScreen} exact />
+      <Route path="/register" component={RegisterScreen} exact />
       <Route path="/mynotes" component={() => <MyNotes />} />
     </main>
     <Footer />
---
+++ client/src/screens/LoginScreen/LoginScreen.js
@@ -0,0 +1,37 @@
+import React from "react";
+import MainScreen from "../../components/MainScreen";
+import { Form, Button, Col, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./LoginScreen.css";
+const LoginScreen = () => {
+  return (
+    <MainScreen title="LOGIN">
+      <div className="loginContainer">
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        <Row className="py-3">
+          <Col>
+            New Customer? <Link to="/register">Register Here</Link>
+          </Col>
+        </Row>
+      </div>
+    </MainScreen>
+  );
+};
+
+export default LoginScreen;
