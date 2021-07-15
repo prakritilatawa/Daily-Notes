7a07460c735183b23ad92bb81d1ef6d8946fb20c
Prakriti Latawa <prakriti.latawa110@gmail.com>
2021-07-15 13:51:43 +0530

    Routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const notes = require("../client/src/data/notes");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const noteRoutes = require("./routes/noteRoutes");
+
 const { notFound, errorHandler } = require("./middlewares/errorMiddlewares");
 const cors = require("cors");
 const app = express();
@@ -22,5 +24,8 @@ connectDB();
 //});
 
 app.use("/api/users", userRoutes);
+app.use("/api/notes", noteRoutes);
+
+app.use(notFound);
 app.use(errorHandler);
 app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
