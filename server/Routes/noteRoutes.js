9934a24d92f314c18e08c21f69c9848c39e6b540
Prakriti Latawa <prakriti.latawa110@gmail.com>
2021-07-20 01:06:43 +0530

    Middleware

--- server/Routes/noteRoutes.js
+++ server/Routes/noteRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
 const { getNotes } = require("../controller/noteController");
+const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 
-router.route("/").get(getNotes);
+router.route("/").get(protect, getNotes);
 
 module.exports = router;
