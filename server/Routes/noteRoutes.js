98ce192f720e02547713bcf509610020ab4c8d8c
Prakriti Latawa <prakriti.latawa110@gmail.com>
2021-07-19 21:20:36 +0530

    Error

--- server/Routes/noteRoutes.js
+++ server/Routes/noteRoutes.js
@@ -1,5 +1,9 @@
 const express = require("express");
+
+const { getNotes } = require("../controller/noteController");
 const router = express.Router();
 
+
+router.route("/").get(getNotes);
 
 module.exports = router;
