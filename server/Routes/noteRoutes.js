9d076d10d91d3409e3700201f94c291cbfba42fc
Prakriti Latawa <prakriti.latawa110@gmail.com>
2021-07-22 14:05:50 +0530

    Delete Update

--- server/Routes/noteRoutes.js
+++ server/Routes/noteRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
-const { getNoteById, getNotes, createNote } = require("../controller/noteController");
+const { getNoteById, getNotes, createNote, UpdateNote, DeleteNote } = require("../controller/noteController");
 const { authMiddleware } = require("../middlewares");
 const router = express.Router();
 
 router.route("/").get(authMiddleware, getNotes);
 router.route("/create").post(authMiddleware, createNote);
 router.route(":id").get(getNoteById);
+router.route("/:id").get(getNoteById).put(authMiddleware, UpdateNote).delete(authMiddleware, DeleteNote);
 
 module.exports = router; 
