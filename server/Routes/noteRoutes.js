50c472734c2a52928d25772fdcd6ff6e3db36f43
Prakriti Latawa <prakriti.latawa110@gmail.com>
2021-07-22 13:48:47 +0530

    API

--- server/Routes/noteRoutes.js
+++ server/Routes/noteRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
-const { getNotes } = require("../controller/noteController");
+const { getNoteById, getNotes, createNote } = require("../controller/noteController");
 const { authMiddleware } = require("../middlewares");
 const router = express.Router();
 
 router.route("/").get(authMiddleware, getNotes);
+router.route("/create").post(authMiddleware, createNote);
+router.route(":id").get(getNoteById);
 
 module.exports = router; 
--- server/controller/noteController.js
+++ server/controller/noteController.js
@@ -2,8 +2,31 @@ const Note = require("../models/noteModel");
 const asyncHandler = require("express-async-handler");
 
 const getNotes = asyncHandler(async (req,res) => {
-    const notes = await Note.find()
+    const notes = await Note.find({user: req.user._id});
     res.json(notes);
 });
 
-module.exports = { getNotes };
+const createNote = asyncHandler(
+    async (req,res) => {
+        const { title, content, category} = req.body;
+        if(!titlr || !content || !category){
+            res.status(400);
+            throw new Error("Please fill all the fields");
+        } else {
+            const note = new Note({ user:req.user._id, title, content, category});
+            const createdNote = await note.save();
+            res.status(201).json(createdNote);
+        }
+    }
+);
+
+const getNoteById = asyncHandler(async (req, res) => {
+    const note = await Note.findById(req.params.id);
+  
+    if (note) {
+      res.json(note);
+    } else {
+      res.status(404).json({ message: "Note not found" });
+    }
+  });
+module.exports = { getNoteById, getNotes, createNote };
